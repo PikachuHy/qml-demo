b1b835520704ac9d75fd8914de64a91c757866b4
PikachuHy <pikachuhy@163.com>
2019-07-16 10:05:53 +0800

    build: add cunit

--- graduate/DataStructuresAndAlgorithmAnalysisInC/main.c
+++ graduate/DataStructuresAndAlgorithmAnalysisInC/main.c
@@ -2,6 +2,61 @@
 // Created by pikachu on 2019/7/16.
 //
 #include <stdio.h>
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+/* The suite initialization function.
+ * Returns zero on success, non-zero otherwise.
+ */
+int init_suite()
+{
+    return 0;
+}
+
+/* The suite cleanup function.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite()
+{
+    return 0;
+}
+
+int maxi(int i1, int i2)
+{
+    return (i1 > i2) ? i1 : i2;
+}
+
+void test_maxi()
+{
+    CU_ASSERT(maxi(0,2) == 2);
+    CU_ASSERT(maxi(0,-2) == 0);
+    CU_ASSERT(maxi(2,2) == 0);
+}
+
+
 int main() {
-    printf("Hello Data Structures\n");
+    CU_pSuite pSuite = NULL;
+
+    /* initialize the CUnit test registry */
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    /* add a suite to the registry */
+    pSuite = CU_add_suite("Suite_1", init_suite, clean_suite);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* add the tests to the suite */
+    if ((NULL == CU_add_test(pSuite, "test of maxi()", test_maxi)) )
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Run all tests using the CUnit Basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
 }
