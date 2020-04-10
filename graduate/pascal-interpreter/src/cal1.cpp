d242d2613e30b63339b8930607293444e02ecd6a
PikachuHy <pikachuhy@163.com>
2020-04-10 16:42:26 +0800

    重构计算器
    将基于表优先级的实现单独放在一个文件里
    增加相关的测试
    目前未实现带有括号的求值

--- graduate/pascal-interpreter/src/cal1.cpp
+++ graduate/pascal-interpreter/src/cal1.cpp
@@ -1,9 +1,150 @@
 #include <string>
 #include <iostream>
 #include <any>
-
+#include <stack>
+#include <vector>
+#include <unordered_set>
+#include "token.h"
 using namespace std;
-#include "interpreter.h"
+class calculator {
+public:
+    calculator(const string &text) : text(text) {}
+    int eval() {
+        std::stack<int> operands;
+        std::stack<token_type> operators;
+        unordered_set<token_type> operators_set = {
+                token_type::plus, token_type::minus,
+                token_type::multiplication, token_type::division
+        };
+        unordered_set<token_type> valid_token_set = operators_set;
+        valid_token_set.insert(token_type::integer);
+        std::vector<std::vector<int>> op_priority_table(4, std::vector<int>(4, 1));
+        op_priority_table[3][0] = -1;
+        op_priority_table[3][1] = -1;
+        op_priority_table[2][0] = -1;
+        op_priority_table[2][1] = -1;
+        std::unordered_map<token_type, int> token_type_index = {
+                {token_type::plus,           0},
+                {token_type::minus,          1},
+                {token_type::multiplication, 2},
+                {token_type::division,       3},
+        };
+
+        cur_token = get_next_token();
+        while (cur_token != token_constant::eof) {
+            operands.push(cur_token.get_value<int>());
+            eat(token_type::integer);
+            if (cur_token == token_constant::eof) break;
+            if (operators_set.find(cur_token.type) != operators_set.end()) {
+                while (!operators.empty()) {
+                    auto row_index = token_type_index[cur_token.type];
+                    auto col_index = token_type_index[operators.top()];
+                    if (op_priority_table[row_index][col_index] > 0) {
+                        auto a = operands.top();
+                        operands.pop();
+                        auto b = operands.top();
+                        operands.pop();
+                        auto op = operators.top();
+                        operators.pop();
+                        auto c = cal(b, a, op);
+                        operands.push(c);
+                    } else break;
+                }
+                operators.push(cur_token.type);
+                eat(cur_token.type);
+            } else {
+                error(valid_token_set);
+            }
+        }
+        while (!operators.empty()) {
+            auto a = operands.top();
+            operands.pop();
+            if (operands.empty()) {
+                error("Invalid syntax", token_type::integer);
+            }
+            auto b = operands.top();
+            operands.pop();
+            auto op = operators.top();
+            operators.pop();
+            auto c = cal(b, a, op);
+            operands.push(c);
+        }
+        return operands.top();
+    }
+
+private:
+    inline void advance() { pos++; }
+    token get_next_token() {
+        if (pos >= text.size()) return token_constant::eof;
+        while (text[pos] == ' ') {
+            advance();
+        }
+        if (isdigit(text[pos])) {
+            auto start = pos;
+            while (isdigit(text[pos])) {
+                advance();
+            }
+            return {token_type::integer, text.substr(start, pos - start), start};
+        }
+        auto iter = token_constant::single_char_token_map.find(text[pos]);
+        if (iter != token_constant::single_char_token_map.end()) {
+            auto ret = iter->second;
+            ret.set_pos(pos);
+            advance();
+            return ret;
+        }
+        return {token_type::unknown, text.substr(pos, 1), pos};
+    }
+    void eat(token_type type) {
+        if (cur_token.type == type) {
+            cur_token = get_next_token();
+            return;
+        }
+        error("Invalid syntax", type);
+    }
+    int cal(int a, int b, token_type type) {
+        switch (type) {
+            case token_type::plus:
+                return a + b;
+            case token_type::minus:
+                return a - b;
+            case token_type::multiplication:
+                return a * b;
+            case token_type::division:
+                return a / b;
+            default:
+                break;
+        }
+        return -1;
+    }
+    void error(string msg, token_type type) {
+        std::cout << "Error: " << msg << std::endl;
+        std::cout << text << std::endl;
+        for(int i=0;i<cur_token.pos;i++) {
+            std::cout << " ";
+        }
+        std::cout << "^" << std::endl;
+        std::cout << "Expect " << type << std::endl;
+        throw msg;
+    }
+    void error(const std::unordered_set<token_type> &set) const {
+        std::cout << "Error type." << std::endl;
+        std::cout << "Expect: ";
+        for (auto it: set) {
+            std::cout << it << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "current type: " << cur_token.type << std::endl;
+        std::cout << "current value: " << cur_token.get_value<string>() << std::endl;
+        exit(1);
+    }
+private:
+    token cur_token;
+    string text;
+    int pos = 0;
+};
+
+#ifndef TEST
 int main() {
     while (true) {
         try {
@@ -11,11 +152,12 @@ int main() {
         string s;
         getline(cin, s);
         if (s.empty()) continue;
-        interpreter inter(s);
-        std::cout << inter.interpret() << std::endl;
+        calculator cal(s);
+        std::cout << cal.eval() << std::endl;
         } catch (std::exception& e) {
             std::cout << e.what() << std::endl;
         }
     }
     return 0;
 }
+#endif
--- graduate/pascal-interpreter/src/interpreter.h
+++ graduate/pascal-interpreter/src/interpreter.h
@@ -18,7 +18,6 @@ using namespace std;
 class interpreter {
 public:
     interpreter(const string& text) : _lexer(lexer(text)), _parser(_lexer) {
-        this->text = text;
     };
 
     int interpret();
@@ -26,30 +25,10 @@ public:
     token get_next_token();
 
     void reset() { _lexer.reset(); }
-private:
-    void eat(token_type type);
-    void error(const std::unordered_set<token_type> & set) const;
-    void error(string msg, token_type type) {
-        std::cout << "Error: " << msg << std::endl;
-        std::cout << text << std::endl;
-        for(int i=0;i<cur_token.pos;i++) {
-            std::cout << " ";
-        }
-        std::cout << "^" << std::endl;
-        std::cout << "Expect " << type << std::endl;
-        throw msg;
-    }
-    int cal(int a, int b, token_type type);
-    int factor();
-    int term();
-    int cal_with_table();
-    int expr();
-    int expr_internal();
+
 private:
     lexer _lexer;
     parser _parser;
-    token cur_token;
-    string text;
     node_visitor visitor;
 };
 
