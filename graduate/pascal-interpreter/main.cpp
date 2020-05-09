1e79cfc64d91e87c4dcd0ac6a79670a60ab32bc3
PikachuHy <pikachuhy@163.com>
2020-05-09 18:21:39 +0800

    添加一个CLI接口

--- graduate/pascal-interpreter/main.cpp
+++ graduate/pascal-interpreter/main.cpp
@@ -2,31 +2,24 @@
 // Created by pikachu on 2020/4/11.
 //
 
-#include <iostream>
-#include <string>
-#include <utility>
-#include <exception>
-#include <vector>
-#include <climits>
-#include <numeric>
-#include <algorithm>
-// https://github.com/gabime/spdlog
-using namespace std;
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include <execinfo.h>
-#include <fmt/format.h>
-#include "parser.h"
-#include "ast_visualizer.h"
-#include <variant>
-int f(int n) {
-    if (n == 0) return 1;
-    return n * f(n-1);
-}
-int main() {
-    for(int i=0;i<17;i++) {
-        std::cout << i << "! =" << f(i) << std::endl;
+#include "interpreter.h"
+#include <spdlog/spdlog.h>
+#include <fstream>
+#include <filesystem>
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        filesystem::path path(argv[1]);
+        if (!filesystem::exists(path)) {
+            SPDLOG_ERROR("file: {} is not exist", path.u8string());
+            return 0;
+        }
+        ifstream fin(argv[1]);
+        stringstream buffer;
+        buffer << fin.rdbuf();
+        string code(buffer.str());
+        interpreter(code).interpret();
+    } else {
+        std::cout << "unimplement" << std::endl;
     }
     return 0;
 }
