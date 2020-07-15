7d9e2b0a22ba8055be654743e54b85328b9c46ab
PikachuHy <PikachuHy@163.com>
2020-07-15 15:27:40 +0800

    消除新API带来的warning

--- graduate/ml-learn/keras-learn/keras_1.py
+++ graduate/ml-learn/keras-learn/keras_1.py
@@ -4,7 +4,8 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+
 tf.logging.set_verbosity(tf.logging.ERROR)
 np.random.seed(1337)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -19,7 +20,7 @@ X_train, Y_train = X[:160], Y[:160]
 X_test, Y_test = X[160:], Y[160:]
 
 model = Sequential()
-model.add(Dense(output_dim=1, input_dim=1))
+model.add(Dense(units=1, input_dim=1))
 # model.add(Dense(output_dim=1))
 model.compile(loss='mse', optimizer='sgd')
 for step in range(301):
