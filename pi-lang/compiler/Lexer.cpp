4da6b0e320e007cb7523581dd46da4a2fc2482e9
PikachuHy <PikachuHy@163.com>
2020-07-20 19:05:22 +0800

    add tokens

--- pi-lang/compiler/Lexer.cpp
+++ pi-lang/compiler/Lexer.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Lexer.h"
+
+#include <utility>
+
+Token::Token(Tag tag) : tag(tag) {
+
+}
+
+Num::Num(int value) : Token(Tag::NUM), value(value) {
+
+}
+
+Word::Word(string lexeme, Tag tag) : Token(tag), lexeme(std::move(lexeme)) {
+
+}
+const Word Word::AND = Word("&&"s, Tag::AND);
+const Word Word::OR = Word("||"s, Tag::OR);
+const Word Word::EQ = Word("=="s, Tag::EQ);
+const Word Word::NE = Word("!="s, Tag::NE);
+const Word Word::LE = Word("<="s, Tag::LE);
+const Word Word::GE = Word(">="s, Tag::GE);
+const Word Word::MINUS = Word("-"s, Tag::MINUS);
+const Word Word::TRUE = Word("true"s, Tag::TRUE);
+const Word Word::FALSE = Word("false"s, Tag::FALSE);
+const Word Word::TEMP = Word("t"s, Tag::TEMP);
+
+Real::Real(float value): Token(Tag::REAL), value(value) {
+
+}
--- pi-lang/compiler/Lexer.h
+++ pi-lang/compiler/Lexer.h
@@ -4,7 +4,8 @@
 
 #ifndef PI_LANG_LEXER_H
 #define PI_LANG_LEXER_H
-
+#include <string>
+using namespace std;
 enum class Tag {
     AND = 256, BASIC, BREAK, DO, ELSE,
     EQ, FALSE, GE, ID, IF,
@@ -12,6 +13,24 @@ enum class Tag {
     OR, REAL, TEMP, TRUE, WHILE
 };
 
+struct Token {
+    Token(Tag tag);
+    const Tag tag;
+};
+struct Num : public Token{
+    Num(int value);
+    const int value;
+};
+struct Word: public Token {
+    Word(string lexeme, Tag tag);
+
+    const string lexeme;
+    const static Word AND, OR, EQ, NE, LE, GE, MINUS, TRUE, FALSE, TEMP;
+};
+struct Real: public Token {
+    Real(float value);
+    const float value;
+};
 class Lexer {
 
 };
