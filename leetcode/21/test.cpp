c39aefb8271873a7f68ec0e379c8d50f0f52034f
PikachuHy <PikachuHy@163.com>
2020-07-20 10:20:05 +0800

    fix number error

--- leetcode/21/test.cpp
+++ leetcode/21/test.cpp
@@ -5,13 +5,13 @@ vector<int> mergeTwoLists(const vector<int>& a, const vector<int>& b) {
     auto ret = s.mergeTwoLists(toListNode(a), toListNode(b));
     return toVector(ret);
 }
-TEST(LeetCode2MergeTwoLists, __LINE__) {
+TEST(LeetCode21MergeTwoLists, __LINE__) {
     vector<int> expect = {1, 1, 2, 2, 3, 3};
     auto ret = mergeTwoLists({1,2,3}, {1, 2, 3});
     EXPECT_EQ(ret, expect);
 }
 
-TEST(LeetCode2MergeTwoLists, __LINE__) {
+TEST(LeetCode21MergeTwoLists, __LINE__) {
     vector<int> expect = {1,2,3};
     auto ret = mergeTwoLists({1}, {2, 3});
     EXPECT_EQ(ret, expect);
--- leetcode/23/test.cpp
+++ leetcode/23/test.cpp
@@ -6,13 +6,13 @@ vector<int> mergeKLists(const vector<vector<int>> lists) {
     auto ret = s.mergeKLists(nodes);
     return toVector(ret);
 }
-TEST(LeetCode2MergeKLists, __LINE__) {
+TEST(LeetCode23MergeKLists, __LINE__) {
     vector<int> expect = {1, 1, 2, 3, 3};
     auto ret = mergeKLists({{1,2}, {3}, {1, 3}});
     EXPECT_EQ(ret, expect);
 }
 
-TEST(LeetCode2MergeKLists, __LINE__) {
+TEST(LeetCode23MergeKLists, __LINE__) {
     vector<int> expect = {1, 1, 2, 2, 2, 4, 5};
     auto ret = mergeKLists({{1,2,2}, {1, 4}, {2, 5}});
     EXPECT_EQ(ret, expect);
