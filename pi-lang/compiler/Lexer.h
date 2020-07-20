bd57ccc327c0d87d21666b2d26c20d2aa0d2d855
PikachuHy <PikachuHy@163.com>
2020-07-20 18:51:55 +0800

    add Tag

--- pi-lang/compiler/Lexer.h
+++ pi-lang/compiler/Lexer.h
@@ -5,6 +5,12 @@
 #ifndef PI_LANG_LEXER_H
 #define PI_LANG_LEXER_H
 
+enum class Tag {
+    AND = 256, BASIC, BREAK, DO, ELSE,
+    EQ, FALSE, GE, ID, IF,
+    INDEX, LE, MINUS, NE, NUM,
+    OR, REAL, TEMP, TRUE, WHILE
+};
 
 class Lexer {
 
