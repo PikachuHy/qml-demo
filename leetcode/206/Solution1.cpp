c89e73eadad13d791886f2321e6f1d661ce3781e
PikachuHy <PikachuHy@163.com>
2020-07-20 14:51:43 +0800

    leetcode 206 Approach 1 Iterative

---
+++ leetcode/206/Solution1.cpp
@@ -0,0 +1,29 @@
+#include "common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *pre = nullptr;
+        auto cur = head;
+        while (cur) {
+            auto next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
+
+#include "test.cpp"
