7a966066040a3efa2779a38bc92e4378d1486135
PikachuHy <PikachuHy@163.com>
2020-07-22 11:03:48 +0800

    leetcode 25 unittest

---
+++ leetcode/25/test.cpp
@@ -0,0 +1,30 @@
+
+#include "gtest/gtest.h"
+Solution s;
+vector<int> reverseKGroup(const vector<int> v, int k) {
+    auto ret = s.reverseKGroup(toListNode(v), k);
+    return toVector(ret);
+}
+TEST(LeetCode25ReverseKGroup, __LINE__) {
+    vector<int> expect = {1, 2};
+    auto ret = reverseKGroup({2,1}, 2);
+    EXPECT_EQ(ret, expect);
+}
+
+TEST(LeetCode25ReverseKGroup, __LINE__) {
+    vector<int> expect = {1, 2, 3};
+    auto ret = reverseKGroup({2,1, 3}, 2);
+    EXPECT_EQ(ret, expect);
+}
+
+TEST(LeetCode25ReverseKGroup, __LINE__) {
+    vector<int> expect = {1, 2, 3, 4};
+    auto ret = reverseKGroup({2,1, 4, 3}, 2);
+    EXPECT_EQ(ret, expect);
+}
+
+TEST(LeetCode25ReverseKGroup, __LINE__) {
+    vector<int> expect = {1, 2, 3, 4, 5};
+    auto ret = reverseKGroup({2,1, 4, 3, 5}, 2);
+    EXPECT_EQ(ret, expect);
+}
