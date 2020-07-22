5dd7fe4fdabe7e2b478018292a7007b17f1d7cdb
PikachuHy <PikachuHy@163.com>
2020-07-22 11:06:22 +0800

    leetcode 25 Approach 1 Recursive

---
+++ leetcode/25/Solution1.cpp
@@ -0,0 +1,37 @@
+#include "common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || !head->next) return head;
+        ListNode dummy(0);
+        auto it = &dummy;
+        it->next = head;
+        for(int i=0;i<k;i++) {
+            if (!it->next) return head;
+            it = it->next;
+        }
+        auto nextNode = it->next;
+        ListNode* prev = nullptr;
+        auto cur = head;
+        while (cur && cur != nextNode) {
+            auto next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head->next = reverseKGroup(nextNode, k);
+        return prev;
+    }
+};
+#include "test.cpp"
