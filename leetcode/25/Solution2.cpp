2254dcbaafebeb27af5093f4ed29753a5a10a74d
PikachuHy <PikachuHy@163.com>
2020-07-22 13:05:32 +0800

    leetcode 25 Approach 2 Iterative

---
+++ leetcode/25/Solution2.cpp
@@ -0,0 +1,52 @@
+#include "common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || !head->next) return head;
+        auto begin = head;
+        auto tail = head;
+        auto it = head;
+        head = nullptr;
+        do {
+            for(int i=0;i<k;i++) {
+                if (!it) {
+                    tail->next = begin;
+                    return head;
+                }
+                it = it->next;
+            }
+            if (head == nullptr) {
+                head = reverseList(begin, it);
+            } else {
+                tail->next = reverseList(begin, it);
+                tail = begin;
+            }
+            begin = it;
+        } while(true);
+        return head;
+    }
+    ListNode* reverseList(ListNode* begin, ListNode* end) {
+        ListNode* prev = nullptr;
+        auto cur = begin;
+        while (cur && cur != end) {
+            auto next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+};
+
+#include "test.cpp"
